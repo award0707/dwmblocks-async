26d2ccbeeff059d55e515fe4a45ac124a2dc3043
Andrew Ward <wardac@vcu.edu>
2025-05-30 18:50:35 -0400

    config

--- config.c
+++ config.c
@@ -4,16 +4,18 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+//    {"sb-mail",    600,  1 },
+//    {"sb-music",   0,    2 },
+    {"sb-cpu",     10,   3 },
+    {"sb-disk",    1800, 4 },
+    {"sb-memory",  10,   5 },
+    {"sb-loadavg", 5,    6 },
+//    {"sb-mic",     0,    7 },
+//    {"sb-record",  0,    8 },
+//    {"sb-netspeed", 1,   9 },
+    {"sb-bt",      60,   9 },
+    {"sb-volume",  0,    10 },
+    {"sb-date",    1,    11 },
 };
 
 const unsigned short blockCount = LEN(blocks);
